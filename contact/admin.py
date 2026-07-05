05126aeba74371615fb68ef4087d57330c239cb9
guihsil <guilherme1234oliveira.go@gmail.com>
2026-07-05 15:42:50 -0300

    django admin page config

--- contact/admin.py
+++ contact/admin.py
@@ -3,4 +3,10 @@ from contact.models import Contact
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = ('first_name', 'last_name', 'phone', 'email',)
+    ordering = ('first_name',)
+    list_filter = ('created_date',)
+    search_fields = ('first_name', 'last_name', 'phone', 'email')
+    list_per_page = 15
+    list_max_show_all = 100
+    list_display_links = ('first_name',)
