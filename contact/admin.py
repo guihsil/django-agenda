cd110694048f9c96d6550be757c5be6eef5c3cf1
guihsil <guilherme1234oliveira.go@gmail.com>
2026-07-06 13:08:00 -0300

    web interface displaying contacts

--- contact/admin.py
+++ contact/admin.py
@@ -3,13 +3,14 @@ from contact.models import Contact, Category
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'phone', 'email',)
-    ordering = ('first_name',)
-    list_filter = ('created_date',)
-    search_fields = ('first_name', 'last_name', 'phone', 'email')
+    list_display = 'first_name', 'last_name', 'phone', 'email', 'show',
+    ordering = 'first_name',
+    list_filter = 'created_date',
+    search_fields = 'first_name', 'last_name', 'phone', 'email'
     list_per_page = 15
     list_max_show_all = 100
-    list_display_links = ('first_name',)
+    list_display_links = 'first_name',
+    list_editable = 'show',
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
