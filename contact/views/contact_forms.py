2014a7e1b38fd5a89550abe557c9b669ea928aed
guihsil <guilherme1234oliveira.go@gmail.com>
2026-07-06 16:11:15 -0300

    initial statement of create page

---
+++ contact/views/contact_forms.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
+from contact.models import Contact
+from django.core.paginator import Paginator
+
+def create(request):
+    context = {
+        'site_title': 'Create - '
+    }
+
+    return render(
+        request,
+        'contact/create.html',
+        context
+    )
